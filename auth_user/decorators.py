72ed22c52d0ee3dcf7146d88ecf06aa990893abc
eliel <elielluwal9@gmail.com>
2023-12-27 23:32:45 +0200

    Mise à jour des permissions de facturation

--- auth_user/decorators.py
+++ auth_user/decorators.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
-            group = None
+            groups = None
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse("Permission non accordée !")
+                groups = request.user.groups.all()
+            for group in groups:
+                if group.name in allowed_roles:
+                    return view_func(request, *args, **kwargs)
+                else:
+                    return HttpResponse("Permission non accordée !")
             return wrapper_func(request, *args, **kwargs)
 
         return wrapper_func
